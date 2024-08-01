4a81cc54d41dd9877a93d071f76bf04cee310b03
Andrea Ceccato <67799925+andreaceccato@users.noreply.github.com>
2024-08-01 18:58:56 +0200

    Add files via upload

--- example.js
+++ example.js
@@ -1,18 +1,10 @@
-# sap-api-js
-Collegamento API SAP con Javascript
+// Collegamento API SAP con Javascript
 
-Using npm:
+// # Esempio di connessione
+// Obbligatori:
+// base
+// credentials
 
-```bash
-$ npm install @ululab/sap
-```
-
-### Esempio di connessione
-
-> **Note** Campi obbligatori oggetto config: 
-> base, credentials
-
-```js
 let config = {
   base: process.env.SAP_BASE_URL,
   credentials: {
@@ -31,29 +23,22 @@ sap.timestampStartSession
 // 1655796768191
 
 sap.timestampExpireSession
-// 1655839968191
+//1655839968191
 
 sap.loginData
-```
-```text
-console.log(sap.loginData)
-{
-  "Version": "1000170",
-  "SessionId": "58f6e39a-f134-11ec-8000-005056a0e477",
-  "SessionTimeout": 720,
-  "odata.metadata": "https://meditsl.gendata.it:50000/b1s/v1/$metadata#B1Sessions/@Element"
-}
-```
+// {
+//    "Version": "1000170",
+//    "SessionId": "58f6e39a-f134-11ec-8000-005056a0e477",
+//    "SessionTimeout": 720,
+//    "odata.metadata": "https://meditsl.gendata.it:50000/b1s/v1/$metadata#B1Sessions/@Element"
+//  }
 
-> **Note** Campo facoltativo oggetto config:
-> loginData.SessionId, con il quale si evita la chiamata di connessione (login)
-```js
 // Salvando questi attributi da qualche parte
 // possiamo recuperarli in seguito per efettuare chiamate rest a sap senza Login
 
 config = {
   base: '',
-  credentials : {...},
+  credentials : {...}
   loginData: {
     SessionId: "58f6e39a-f134-11ec-8000-005056a0e477",
   },
@@ -66,14 +51,17 @@ sap = await Sap.connect(config);
 await sap.get('BusinessPartners');
 
 // Sap ritorna le collezioni con un oggetto a due chiavi all'interno della chiave value
-sap.responseData 
+sap.responseData
 // { odata.metadata: '...', value: [..] } array: collezione di oggetti
 
 sap.getResponse()
-// ritorna l'attributo responseData
+// ritorna il tutto valore della risposta sap intressato:
+// se collezione: sap.responseData.value, ritornando un array di oggetti
+// se single type: sap.responseData, ritornando un oggetto
 
 sap.response()
 // ritorna in modo intelligente il valore della chiamata rest (che sia un acollezione o una single type)
+// utilizza il metodo getResponse()
 
 // Sap ritorna i singoli record direttamente con l'oggetto contenente tutti gli attributi dell'entitá
 await sap.get(`BusinessPartners('CA0B01)`);
@@ -81,16 +69,13 @@ await sap.get(`BusinessPartners('CA0B01)`);
 // { CardCode: 'CA0B01', CardType: 'cLid', ... }
 
 sap.response()
-```
 
-Se vuoi puoi acccedere direttamente ai valori dell'oggetto response passando il percorso del campo
-```js
+// Se vuoi puoi acccedere direttamente ai valori dell'oggetto response passando il percorso del campo
 sap.response('CardCode')
 sap.response('BPPaymentMethods.0.PaymentMethodCode')
 sap.response('BPIntrastatExtension.TransportMode')
-```
-### Filtri e selezioni campi
-```js
+
+// # Filtri e selezioni campi
 let select = [
   'CardName',
   'CardCode',
@@ -111,7 +96,6 @@ let update = {
 }
 
 await sap.get('BusinessPartners', {
-  $select: select.join(','), // array o stringhe separate da virgola
+  $select: select.join(','),
   $filter: `UpdateDate ge '${update.date}' and UpdateTime ge '${update.time}'`
 });
-```
